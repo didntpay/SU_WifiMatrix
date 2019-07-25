6b60dfda25dde65b979ced3f6ea492e296f14df5
Weifeng Li <weifengli2014@gmail.com>
2019-07-25 16:37:35 -0700

    Demo

--- LEDController/Form1.Designer.cs
+++ LEDController/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.connect = new System.Windows.Forms.Button();
+            this.TargetIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Port_Num = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(217, 60);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // TargetIp
+            // 
+            this.TargetIp.Location = new System.Drawing.Point(30, 40);
+            this.TargetIp.Name = "TargetIp";
+            this.TargetIp.Size = new System.Drawing.Size(165, 22);
+            this.TargetIp.TabIndex = 1;
+            this.TargetIp.Text = "1.1.1.2";
+            this.TargetIp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address";
+            // 
+            // Port_Num
+            // 
+            this.Port_Num.Location = new System.Drawing.Point(30, 85);
+            this.Port_Num.Name = "Port_Num";
+            this.Port_Num.Size = new System.Drawing.Size(165, 22);
+            this.Port_Num.TabIndex = 3;
+            this.Port_Num.Text = "12345";
+            this.Port_Num.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Port_Num);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TargetIp);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.TextBox TargetIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Port_Num;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- LEDController/Form1.cs
+++ LEDController/Form1.cs
@@ -4,17 +4,69 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 
 namespace LEDController
 {
     public partial class Form1 : Form
     {
+        public Socket ledconnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            String targetip = TargetIp.Text;
+            int portnum = Convert.ToInt32(Port_Num.Text);
+            if (ledconnection.Connected)
+            {
+                endConnection(ledconnection);
+                //log it, and tell user to try again.
+            }
+            else
+            {
+                try
+                {
+                    IPEndPoint tmpEP = new IPEndPoint(IPAddress.Parse(targetip), portnum);
+                    ledconnection.Connect(tmpEP);
+                    sendMessage(ledconnection, "YO, turn on my guy");
+                }
+                catch(SocketException se)
+                {
+                    //do nothing for now.
+                }
+            }
+        }
+
+        private void isIPValid()//implement this later and run before connect.
+        {
+
+        }
+
+        private void sendMessage(Socket connection, String mes)
+        {
+            Byte[] buffer = Encoding.ASCII.GetBytes(mes);
+            connection.Send(buffer, SocketFlags.None);
+        }
+
+        private void endConnection(Socket connection)
+        {
+            if (!connection.Connected)
+            {
+                //something went wrong, log it.
+            }
+            else
+            {
+                connection.EndConnect((IAsyncResult)connection);
+            }
         }
     }
 }
